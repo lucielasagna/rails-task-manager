0fb676d51ac8f49ac6a5cfb690a0e94f6e4030e1
Lucie La Sagna <lucie.lasagna@essec.edu>
2017-02-14 16:31:24 +0100

    exo1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get "tasks", to: "tasks#index"
+  # post 'tasks', to: 'tasks#create'
+  # get "tasks/new" => "tasks#new", as: :new_restaurant
+  # get '/tasks/:id/edit' => 'tasks#edit', as: :edit_restaurant
+  # get "tasks/:id", to: "tasks#show"
+  # patch '/tasks/:id' => 'tasks#update'
+  # delete '/tasks/:id' => 'tasks#destroy'
+
+  resources(:tasks)
 end
